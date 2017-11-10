6585a8d8a6d5fa9657ebc4930b3133b014a8169c
Fabiano Alencar <fabianocarvalho15@gmail.com>
2017-11-10 16:54:34 -0200

    Mudado para Select e melhorado a função!

--- functions.php
+++ functions.php
@@ -31,29 +31,18 @@ function objetos_metabox_callback(){
     wp_nonce_field( $post->ID, 'objetos_metabox_nonce' );
     $relacionados =  wp_get_recent_posts( [ 'post_type' => 'socialdb_object', 'numberposts' => -1, 'post_status' => 'publish', 'orderby' => 'post_title', 'order' => 'DESC'] );
     $stored_meta = get_post_meta( $post->ID, 'objetos_metabox' );
+    //var_dump($relacionados); die;
     ?>
-    <div style="max-height: 201px; overflow: auto;">
-        <?php
-            foreach ($relacionados as $rel) {
-            //var_dump($relacionados); die;
-            ?>   
-                <input type="checkbox" name="objetos_metabox[]" id="objetos_<?php echo $rel['ID']; ?>" value="<?php echo $rel['ID']; ?>" 
-                    <?php 
-                        if(isset($stored_meta[0][0])) {
-                            $count = count($stored_meta[0][0]) - 1;
-                            for($x=0; $x<=$count; $x++){
-                                checked( $stored_meta[0][0][$x], $rel['ID'] );
-                            }
-                        }
-                    ?>
-                > 
-                <label for="objetos_<?php echo $rel['ID']; ?>" style="margin-top: 5px; margin-bottom: 1px;">
-                    <?php echo $rel['post_title']; ?>
-                </label>
-                <br>
+    <div>
+        <select>
             <?php
-            }
-        ?>
+                foreach ($relacionados as $rel) {
+                ?>   
+                    
+                        <option value="<?php echo $rel['ID']; ?>" <?php selected( $stored_meta[0], $rel['ID'] ); ?> > <?php echo $rel['post_title']; ?> </option>
+                    <br>
+                <?php } ?>
+        </select>
     </div>
 <?php
 }
@@ -72,7 +61,7 @@ function objetos_metabox_save( $post_id ) {
  
     // Checks for input and sanitizes/saves if needed
     if( isset( $_POST[ 'objetos_metabox' ] ) ) {
-        update_post_meta( $post_id, 'objetos_metabox', array($_POST[ 'objetos_metabox' ]) );
+        update_post_meta( $post_id, 'objetos_metabox', $_POST[ 'objetos_metabox' ] );
     }
  
 }
@@ -97,38 +86,18 @@ function tainacan_fc_get_post_meta($meta_key) {
     }
 }
 
-/*
-* Function que verifca se o objeto esta relacionado ao post no postmeta
-* @param $object_id id do objeto
-*/
-function has_relation($object_id){
-    $stored_meta = tainacan_fc_get_post_meta( 'objetos_metabox' );
-    foreach ($stored_meta as $value) {
-        $postmeta = get_post_meta( $value->post_id, 'objetos_metabox', true );
-
-        if($postmeta[0][0] == $object_id)
-            $x = true;
-        else
-            $x = false;
-    }
-    if($x)
-        return true;
-    else
-        return false;
-}
-
 function objetos_list($object_id){
     $stored_meta = tainacan_fc_get_post_meta( 'objetos_metabox' );
-    
-    if(!empty($stored_meta) && has_relation($object_id)){ ?>
-    
-        <h3 id="text_title">
-            <?php _e('Posts Relacionados', 'tainacan'); ?>
-            <br>
-        </h3>    
-        <hr>
-        <?php
-        foreach ($stored_meta as $meta) { ?>
+    $x=0;
+    foreach ($stored_meta as $meta) { 
+        //var_dump($meta);
+        if($meta->meta_value == $object_id) { $x++;
+        ?>
+            <h3 id="text_title" style="<?php if($x==0 || $x > 1) echo 'display: none';?>">
+                <?php _e('Posts Relacionados', 'tainacan'); ?>
+                <br>
+            </h3>    
+            <hr style="<?php if($x==0 || $x > 1) echo 'display: none';?>">
             <h4 style="border-bottom: 1px dashed #CCCCCC; padding-bottom: 8px; width: 80%; margin: 10px auto;">
                 <?php if(has_post_thumbnail( $meta->post_id )) { ?>    
                     <img src="<?php echo get_the_post_thumbnail_url( $meta->post_id, $size = 'post-thumbnail' ); ?>" alt="" class="img-responsive" style="display: inline-block; max-width: 45px; float: left; margin-right: 5px;"/>
@@ -137,9 +106,20 @@ function objetos_list($object_id){
                     <?php echo get_the_title( $meta->post_id ); ?>
                 </a>
             </h4> 
-        <?php } ?>
+        <?php } 
+    } ?>
     <hr>
-    <?php }
+    <?php 
 }
 
 add_action( 'header_sidebar_item', 'objetos_list');
+
+function list_objects_in_single($content){
+    global $post;
+    $relacionados =  wp_get_recent_posts( [ 'post_type' => 'socialdb_object', 'numberposts' => -1, 'post_status' => 'publish', 'orderby' => 'post_title', 'order' => 'DESC'] );
+    $stored_meta = get_post_meta( $post->ID, 'objetos_metabox' );
+    var_dump($stored_meta);
+    echo '<div style="position: absolute; top: 10px; right: 10px;">Post relacionado a '.$stored_meta[0][0][0].'</div>';
+    return $content;
+}
+add_filter( 'the_content', 'list_objects_in_single');
