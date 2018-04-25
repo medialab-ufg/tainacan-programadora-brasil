767e4cf2f0c97198cca4372c216961ef97efe739
Fabiano Alencar <fabianocarvalho15@gmail.com>
2018-04-25 12:17:42 -0300

    change the number of posts

--- front-page.php
+++ front-page.php
@@ -37,7 +37,7 @@ get_template_part("header","front");
         </div>
         <div class="home-container carousel-home">
             <?php
-                $carmen =  get_children( array('post_parent' => 24, 'numberposts' => 500) );
+                $carmen =  get_children( array('post_parent' => 24) );
                 //var_dump($child_post);die;
                 //$child_items =  wp_get_recent_posts( [ 'post_type' => 'socialdb_object', 'numberposts' => -1, 'post_status' => 'publish', 'orderby' => 'post_title', 'order' => 'DESC'] );
                 foreach ($carmen as $item):
@@ -74,7 +74,7 @@ get_template_part("header","front");
         </div>
         <div class="home-container carousel-home catalogofilmes">
             <?php
-                $filmes =  get_children( array('post_parent' => 38) );
+                $filmes =  get_children( array('post_parent' => 38, 'numberposts' => 500) );
                 foreach ($filmes as $item):
                     ?>
                     <div class="col-md-12 col-sm-12 featured" style="width: 215px !important;">
